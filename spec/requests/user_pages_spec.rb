3774661a4b5c443c6f32d9a21f4ca4e37bb86341
StefenClausen <stefanclaussen@me.com>
2014-01-15 10:35:38 +0000

    Finish user edit, update, index, and destroy actions.  End of chapter 9, failing tests.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,11 +5,10 @@ describe "User pages" do
   subject { page }
 
   describe "index" do
+    
     let(:user) { FactoryGirl.create(:user) }
 
     before do
-      sign_in FactoryGirl.create(:user)
-      before(:each) do
         sign_in user
         visit users_path
     end
@@ -24,13 +23,34 @@ describe "User pages" do
 
       it { should have_selector('div.pagination') }
 
-    it "should list each user" do
-      User.all.each do |user|
-        expect(page).to have_selector('li', text: user.name)
+      it "should list each user" do
+        User.all.each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
   end
-end
 
   describe "profile page" do
   	let(:user) { FactoryGirl.create(:user) }
@@ -111,5 +131,4 @@ end
       end
     end
   end
-end
 end
